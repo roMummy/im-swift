1e36e3302f1cd2ac237be2a8857730173bc272da
roMummy <tianwen520@outlook.com>
2025-12-15 15:27:10 +0800

    添加spm 示例

---
+++ SPMExample/SPMExample/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  SPMExample
+//
+//  Created by macbook pro 2022 m2 on 2025/12/15.
+//
+
+import SwiftUI
+import IMSDK
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button.init {
+                convertToPNG()
+            } label: {
+                Text("Convert Image")
+            }
+        }
+        .padding()
+    }
+    
+    private func convertToPNG() {
+        let input = Bundle.main.path(forResource: "111", ofType: "jpg")!
+        let output = NSTemporaryDirectory() + "output.png"
+        
+        let result = IMCore.shared.conver(inputPath: input, outputPath: output)
+        
+        IMCore.shared.progressBlock = { value in
+            print("Progress: \(value)")
+        }
+        
+        print(result.status == .success)
+        
+        print(output)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SPMExample/SPMExample/SPMExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPMExampleApp.swift
+//  SPMExample
+//
+//  Created by macbook pro 2022 m2 on 2025/12/15.
+//
+
+import SwiftUI
+
+@main
+struct SPMExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
